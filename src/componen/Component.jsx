ff3fc0035248b16d2c48fbe47114b9abb015c35c
nizam <nizudin120@gmail.com>
2023-11-06 16:55:35 +0530

    toggle change

--- src/componen/Component.jsx
+++ src/componen/Component.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import img from '../Image/img1.png'
+import { useState } from 'react'
 
 function Component() {
+    const [toggle, setToggle] = useState(false)
+    // const [select, setSelect] = useState(1)
+
+    const benifits = () => {
+        setToggle(false)
+        // setSelect()
+    }
+
+    const orginization = () => {
+        setToggle(true)
+    }
+
+
+
     return (
         <div>
             <div className="container d-flex justify-content-center ">
@@ -10,7 +25,8 @@ function Component() {
                         <h3 className='header1'>Why Leading SAFe Certification Training ?</h3>
 
                         <hr />
-                        <p className='Content'>Nowadays, most of the companies are looking for SAFe certified professionals, as SAFe certified professionals can easily and rapidly adapt to the Agile transformation in an organization. So, irrespective of whether you want to get promoted in your current organization or you want to get a new job, you must acquire SAFe Agilist certification that is globally recognized and adds great value to your profile and will certainly enhance your career.</p>
+                        <p className='Content'>Nowadays, most of the companies are looking for SAFe certified professionals, as SAFe certified professionals can easily and rapidly adapt to the Agile transformation in an
+                            organization. So, irrespective of whether you want to get promoted in your current organization or you want to get a new job, you must acquire SAFe Agilist certification that is globally recognized and adds great value to your profile and will certainly enhance your career.</p>
                     </div>
                     <div className="footer d-flex justify-content-between">
                         <div className="img">
@@ -19,19 +35,38 @@ function Component() {
                         <div className="container1">
                             <div className="f-content p-2 m-2 ">
                                 <div className="btn d-flex justify-content-between m-2">
-                                    <button  className="bt text-uppercase">Benefits</button>
-                                    <button  className="bt1 text-uppercase">How Organisations Benefits</button>
+                                    <button className={`bt text-uppercase  ${!toggle ? 'active' : ''}`} onClick={e => benifits(e)} >Benefits</button>
+                                    <button className={`bt1 text-uppercase ${toggle ? 'active' : ''}`} onClick={e => orginization(e)} >How Organisations Benefits</button>
                                 </div>
                             </div>
-                            <div className="list mx-5">
-                                <ul>
-                                    <li>Gain a competitive edge over your peers</li>
-                                    <li>Build professional credibility and increase your earning potential</li>
-                                    <li>Grab more career opportunities</li>
-                                    <li>Keep up with the fast-changing market trends</li>
-                                    <li>Enhances the skill set and knowledge you currently possess</li>
-                                </ul>
-                            </div>
+
+                            {
+                                !toggle  ?
+
+                                    <div className="list mx-5">
+                                        <ul>
+                                            <li>Gain a competitive edge over your peers</li>
+                                            <li>Build professional credibility and increase your earning potential</li>
+                                            <li>Grab more career opportunities</li>
+                                            <li>Keep up with the fast-changing market trends</li>
+                                            <li>Enhances the skill set and knowledge you currently possess </li>
+
+                                        </ul>
+                                    </div>
+
+                                    :
+                                    <div className="list mx-5">
+                                        <ul>
+                                            <li>Hello</li>
+                                            <li>Byee</li>
+                                            <li>Dont know</li>
+                                            <li>Keep up with them</li>
+                                            <li>Lorem ipsum dolor sit amet consectetur  </li>
+
+                                        </ul>
+                                    </div>
+
+                            }
                         </div>
                     </div>
                 </div>
@@ -42,3 +77,5 @@ function Component() {
 }
 
 export default Component
+
+
